ee6fa877bd5dac8499337a54c323596a5fc3e159
seo-04 <coups1234@naver.com>
2025-08-12 09:50:53 +0900

    springbootwebprj 웹
    구성

---
+++ springbootwebprj/src/main/java/com/mjc813/springbootwebprj/SpringbootwebprjApplication.java
@@ -0,0 +1,13 @@
+package com.mjc813.springbootwebprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebprjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwebprjApplication.class, args);
+    }
+
+}
---
+++ springbootwebprj/src/test/java/com/mjc813/springbootwebprj/SpringbootwebprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjc813.springbootwebprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebprjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
